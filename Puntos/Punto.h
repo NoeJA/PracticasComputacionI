63919c4455579d735e8cb25143f1892abfa380b4
NoeJA <71842752+NoeJA@users.noreply.github.com>
2020-11-04 17:41:08 -0600

    Update Punto.h

--- Puntos/Punto.h
+++ Puntos/Punto.h
@@ -3,36 +3,19 @@
 #include <cmath>
 
 class Punto2D{
-    float x,y;
+	private:
+    	float x{0},y{0};
 public:
-    void SetPosicion(float n1,float n2){
-        x=n1;
-        y=n2;
-    }
+    void SetPosicion(float n1,float n2);
 
-    void Trasladar(float temp1,float temp2){
-        x+=temp1;
-        y+=temp2;
-    }
-    void RotarRespectoAlOrigen(float temp1){
-        temp1=temp1*(3.14159/180);
-        float xp=x*(cos(temp1))-y*(sin(temp1));
-        float yp=y*(cos(temp1))+x*(sin(temp1));
-        x=xp;
-        y=yp;
-    }
-    void Escalar(float temp1, float temp2 ){
-        x*=temp1;
-        y*=temp2;
-    }
-    float GetX(){
-        return x;
-    }
-    float GetY(){
-        return y;
-    }
+    void Trasladar(float temp1,float temp2);
+    void RotarRespectoAlOrigen(float temp1);
+    void Escalar(float temp1, float temp2 );
+    float GetX();
+    float GetY();
 
 };
 
 #endif //PUNTOS_LIBRARY_H
+
 
